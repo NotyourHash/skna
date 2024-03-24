835dd1c0ea05397fd4f996f5f48020bdfd22a304
NotyourHash <146540249+NotyourHash@users.noreply.github.com>
2024-03-24 22:16:05 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "2107529793").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/stkeditz/HarshuXDMUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/NotyourHash/skna")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LOVE_FEELINGS_WILL")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/LOVE_FEELINGS_WILL")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Equinox_Chats")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Equinox_Chats")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
  
