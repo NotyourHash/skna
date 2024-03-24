037dc01e16215e0d509c24a70234e837e9de3ef1
NotyourHash <146540249+NotyourHash@users.noreply.github.com>
2024-03-24 18:23:19 +0530

    Add files via upload

---
+++ HarshuXD/__init__.py
@@ -0,0 +1,41 @@
+from HarshuXD.core.bot import HarshuXDBot
+from HarshuXD.core.dir import dirr
+from HarshuXD.core.git import git
+from HarshuXD.core.userbot import Userbot
+from HarshuXD.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = HarshuXDBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
